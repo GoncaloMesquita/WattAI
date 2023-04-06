f049129d368ec392fd151b8ae3b7a0f7dae68632
GoncaloMesquita <goncalos.mesq543@gmail.com>
2023-04-06 13:15:26 +0100

    zimbora

--- Environment/mlp_operational_data.py
+++ Environment/mlp_operational_data.py
@@ -37,22 +37,22 @@ class MLP(nn.Module):
         self.fc1 = nn.Linear(input_size, hidden_size1)
         self.relu = nn.ReLU()
         self.dropout1 = nn.Dropout(dropout_prob)
-        # self.fc2 = nn.Linear(hidden_size1, hidden_size2)
-        # self.relu = nn.ReLU()
-        # self.dropout2 = nn.Dropout(dropout_prob)
+        self.fc2 = nn.Linear(hidden_size1, hidden_size2)
+        self.relu = nn.ReLU()
+        self.dropout2 = nn.Dropout(dropout_prob)
         # self.fc3 = nn.Linear(hidden_size2, hidden_size3)
         # self.relu = nn.ReLU()
         # self.dropout3 = nn.Dropout(dropout_prob)
         # self.fc4 = nn.Linear(hidden_size3, output_size)
-        self.fc4 = nn.Linear(hidden_size1, output_size)
+        self.fc4 = nn.Linear(hidden_size2 , output_size)
 
     def forward(self, x):
         x = self.fc1(x)
         x = self.relu(x)    
         x = self.dropout1(x)
-        # x = self.fc2(x)
-        # x = self.relu(x)
-        # x = self.dropout2(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.dropout2(x)
         # x = self.fc3(x)
         # x = self.relu(x)
         # x = self.dropout3(x)
@@ -66,8 +66,8 @@ class MLP(nn.Module):
 
 def run_model(x_norm_train,y_train, x_norm_test, y_test ):
     input_size =11
-    hidden_size1 = 80
-    hidden_size2 = 10
+    hidden_size1 = 16
+    hidden_size2 = 8
     hidden_size3 = 10
     dropout_prob = 0
     output_size = 1
@@ -95,21 +95,17 @@ def run_model(x_norm_train,y_train, x_norm_test, y_test ):
         y_pred = model(torch.tensor(x_norm_test).float()) 
         print(' Testing -> Loss: {:.4f}, R2: {:.4f}'.format( criterion(y_pred, torch.tensor(y_test.values).float()).item() , r2_score1(torch.tensor(y_test.values).float() ,y_pred)))
     
-
-    print('R2:', r2_score(y_test.to_numpy(), y_pred.numpy() ))
     return y_pred
 
 
 
 ##################################################### Main #####################################################
 
-# train_data = pd.read_csv('Synthetic_data/clean_synthetic_data.csv')
-validation_data = pd.read_csv('Training_data.csv')
-# df = pd.concat([train_data, validation_data], axis=0, ignore_index=True)
+df = pd.read_csv('dataset_building.csv')
 
-y_ns= pd.DataFrame(validation_data.loc[:,['indoor_temp_interior', 'co2','supply_air_temp', 'return_air_temp', 'filtered_air_flow_rate']].values, columns=['ns_indoor_temp_interior', 'ns_co2','ns_supply_air_temp', 'ns_return_air_temp', 'ns_filtered_air_flow_rate'])
+y_ns= pd.DataFrame(df.loc[:,['indoor_temp_interior', 'co2','supply_air_temp', 'return_air_temp', 'filtered_air_flow_rate']].values, columns=['ns_indoor_temp_interior', 'ns_co2','ns_supply_air_temp', 'ns_return_air_temp', 'ns_filtered_air_flow_rate'])
 
-df = validation_data.iloc[:-1]
+df = df.iloc[:-1]
 x_ns = y_ns.iloc[1:]
 x_ns = x_ns.reset_index(drop=True)
 y_ns = y_ns.iloc[:-1]
@@ -127,10 +123,14 @@ df_ns.to_csv('Environment/data_set_environment.csv', index=False)
 
 x_train, x_test, Y_train, Y_test = train_test_split(x_action_state , y_ns , test_size=0.15, random_state=42)
 
+Training = pd.concat([x_train, Y_train ], axis=1, ignore_index=False)
+Training.to_csv('Environment/training_environment.csv', index=False)
+
+Testing = pd.concat([x_test, Y_test ], axis=1, ignore_index=False)
+Testing.to_csv('Environment/testing_environment.csv', index=False)
+
 X_norm_test, X_norm_train = normalize_data(x_train, x_test)
 
 y_ns_pred = run_model(X_norm_train,Y_train, X_norm_test, Y_test)
-
-print(y_ns_pred.numpy(), Y_test.to_numpy())
 
 
