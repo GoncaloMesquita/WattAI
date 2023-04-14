d294ee1de70903b6512b40c5ba5e27aacbfaa8b6
GoncaloMesquita <goncalos.mesq543@gmail.com>
2023-04-14 03:51:31 +0100

    zimbora

--- Environment/mlp_operational_data.py
+++ Environment/mlp_operational_data.py
@@ -180,7 +180,6 @@ if __name__ == '__main__':
     df_ns = pd.concat([df, x_ns ], axis=1, ignore_index=False)
     df_ns.to_csv('Environment/data_set_environment.csv', index=False)
 
-<<<<<<< HEAD
     x_action = df.loc[:,['zone_temp_cooling', 'zone_temp_heating', 'supplyfan_speed', 'returnfan_speed', 'outdoor_air_damper_position', 'Outdoor_temp']]
     x_action_state = pd.concat([x_action, x_ns], axis=1)   
 
@@ -201,58 +200,62 @@ if __name__ == '__main__':
 
     # #######################################  Enviorment MODEL   ######################################
 
-    y_ns_pred, model_next_state = run_model_enviorment(X_norm_train,Y_train.loc[:,['indoor_temp_interior', 'co2']], X_norm_test, Y_test.loc[:,['indoor_temp_interior', 'co2']], 2)
-
-    # #######################################  Air Temp MODEL   #############################################
-
-    y_operational_data_pred, model_air_temp_suplly_return  = run_model_air_temp(X_norm_train , Y_train.loc[:,['supply_air_temp', 'return_air_temp']], X_norm_test, Y_test.loc[:,['supply_air_temp', 'return_air_temp']], 2)
-
-    # #######################################  Air Flow rate model #########################################
-
-    y_operational_data_pred, model_air_flowrate  = run_model_air_flowrate(X_norm_train , Y_train.loc[:,['filtered_air_flow_rate']], X_norm_test, Y_test.loc[:,['filtered_air_flow_rate']], 1 )
-=======
-    x_train, x_test, Y_train, Y_test = train_test_split(x_action_state , df.loc[:,['indoor_temp_interior', 'co2','supply_air_temp', 'return_air_temp', 'filtered_air_flow_rate']], test_size=0.15, random_state=42)
-
-
-    Training = pd.concat([x_train, Y_train ], axis=1, ignore_index=False)
-    Training.to_csv('Environment/training_environment.csv', index=False)
-
-    Testing = pd.concat([x_test, Y_test ], axis=1, ignore_index=False)
-    Testing.to_csv('Environment/testing_environment.csv', index=False)
-
-    X_norm_test, X_norm_train = normalize_data(x_train, x_test)
-
-    # #######################################  Enviorment MODEL   ######################################
-
-    y_ns_pred, model_next_state = run_model_enviorment(X_norm_train,Y_train.loc[:,['indoor_temp_interior', 'co2']], X_norm_test, Y_test.loc[:,['indoor_temp_interior', 'co2']], 2)
+    # y_ns_pred, model_next_state = run_model_enviorment(X_norm_train,Y_train.loc[:,['indoor_temp_interior', 'co2']], X_norm_test, Y_test.loc[:,['indoor_temp_interior', 'co2']], 2)
 
 
     # #######################################  Air Temp MODEL   #############################################
 
-    y_operational_data_pred, model_air_temp_suplly_return  = run_model_air_temp(X_norm_train , Y_train.loc[:,['supply_air_temp', 'return_air_temp']], X_norm_test, Y_test.loc[:,['supply_air_temp', 'return_air_temp']], 2)
+    # y_operational_data_pred, model_air_temp_suplly_return  = run_model_air_temp(X_norm_train , Y_train.loc[:,['supply_air_temp', 'return_air_temp']], X_norm_test, Y_test.loc[:,['supply_air_temp', 'return_air_temp']], 2)
 
     # #######################################  Air Flow rate model #########################################
 
-    y_operational_data_pred, model_air_flowrate  = run_model_air_flowrate(X_norm_train , Y_train.loc[:,['filtered_air_flow_rate']], X_norm_test, Y_test.loc[:,['filtered_air_flow_rate']], 1 )
+    # y_operational_data_pred, model_air_flowrate  = run_model_air_flowrate(X_norm_train , Y_train.loc[:,['filtered_air_flow_rate']], X_norm_test, Y_test.loc[:,['filtered_air_flow_rate']], 1 )
 
->>>>>>> bb82c8ead90cd85b3175a87b0b23eda2c211ca6e
 
     # # Save the models 
 
-<<<<<<< HEAD
     # # Save the models 
 
-=======
->>>>>>> bb82c8ead90cd85b3175a87b0b23eda2c211ca6e
     # torch.save(model_next_state, 'Environment/model_next_state.pt')
     # torch.save(model_air_temp_suplly_return, 'Environment/model_air_temp_suplly_return.pt')
     # torch.save(model_air_flowrate, 'Environment/model_air_flowrate.pt')
 
-df_databuilding = pd.read_csv('dataset_building.csv')
+df_building= pd.read_csv('dataset_building.csv')
 
 syntetich = pd.read_csv( 'Synthetic_data/synthetic_data.csv' )
 
-df_concat = pd.concat([df_databuilding, syntetich ], axis=0)
+# df_concat = pd.concat([df_databuilding, syntetich ], axis=0)
 
 
-print(df_concat['co2'].describe())
+# print(df_concat['co2'].describe())
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+reward = []
+
+thermal_comfort=[]
+CO2_comfort=[]
+# for i in range (len(df_building['indoor_temp_interior'])):
+
+std_T = 2
+thermal_comfort.append(np.exp(-(df_building.iloc[:,4]-21)**2/(2*std_T**2))/(std_T*np.sqrt(2*np.pi)))
+CO2_comfort.append(-1/(1+np.exp(-0.2*(df_building.iloc[:,5]-700)))+1)
+
+max_thermal_comfort = 0.199
+# Reward calculation
+for i in range (len(thermal_comfort)):
+    reward.append(-.5*(df_building.iloc[:,0]) /130 + .35*thermal_comfort[i]/max_thermal_comfort + .15*CO2_comfort[i])
+
+print(thermal_comfort[i])
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(-.5*(df_building.iloc[:,0]) /130 , .35*np.array(thermal_comfort)/max_thermal_comfort + .15*np.array(CO2_comfort), reward[0:1000])
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+ax.set_title('3D Scatter Plot')
+# plt.scatter(reward,.35*thermal_comfort/max_thermal_comfort + .15*CO2_comfort)
+# plt.scatter(reward,-.5*(df_building.iloc[:,0]) /130 )
+
+plt.show()
